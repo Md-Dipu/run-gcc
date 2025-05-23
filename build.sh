178f81aec6982590cb66d69d4db7e0c7eb58918e
Md. Dipu <srdipu@hotmail.com>
2025-05-24 00:56:11 +0600

    fix(build): update docker-compose command to docker compose

--- build.sh
+++ build.sh
@@ -10,10 +10,10 @@ TARGET=$1
 
 if [ "$TARGET" = "deb" ]; then
   echo "Building Debian package using debuild..."
-  docker-compose run --rm debian-builder
+  docker compose run --rm debian-builder
 elif [ "$TARGET" = "arch" ]; then
   echo "Building Arch package using makepkg..."
-  docker-compose run --rm arch-builder
+  docker compose run --rm arch-builder
 else
   echo "Unknown target: $TARGET"
   echo "Usage: $0 {deb|arch}"
